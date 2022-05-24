5f824e22890343c2519443bdc469aeb16b309cdc
dev_paul <yjk@marpple.com>
2022-05-24 14:22:04 +0900

    middleware 함수 정리

--- utils/middleWares.ts
+++ utils/middleWares.ts
@@ -5,16 +5,5 @@ export const authMiddleWare =
   (cb: (ctx: GetServerSidePropsContext, isValid: boolean) => object) =>
   async (ctx: GetServerSidePropsContext) => {
     const cookies = cookieParser<{ email?: string }>(ctx.req.headers?.cookie)
-    // const cookies = cookieParser<{ userId?: string; userName?: string; }>("userId=1; userName=youngju;");
-    // const cookies: any = cookieParser(undefined)
-
-    console.log('cookies: ', cookies)
-    console.log('cookies2: ', ctx.req.headers?.cookie)
-    console.log('req.header: ', ctx.req.headers)
-
-    if ('email' in cookies) {
-      return cb(ctx, true)
-    } else {
-      return cb(ctx, false)
-    }
+    return cb(ctx, 'email' in cookies)
   }
